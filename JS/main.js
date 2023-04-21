3b97de60fee47b2bfd382a79e052ad9dbcd31a03
maksbovtuta <klarus335@ukr.net>
2023-04-21 15:24:54 +0300

    add gitignor

--- JS/main.js
+++ JS/main.js
@@ -1,5 +1,5 @@
 var app = new Vue({
-    el:".tovar-tomato",
+    el:".tovar-tomato, .main-tomato-one",
     data:{
         products:
         [{
@@ -7,12 +7,14 @@ var app = new Vue({
             title:"Articul: 10112",
             short_text:'Stardant Orange',
             href:'tomato-one.html',
+            img:'standart.jpg',
             desc:"Full desc"
         },
         {   id:"tomatos-2", 
             title:"Articul: 10123", 
             short_text:'Cintrag Orange', 
             href:'#',
+            img:'cintrag-orange.jpg',
             desc:"Full desc"
         },    
         {   
@@ -20,6 +22,7 @@ var app = new Vue({
             title:"Articul: 11232", 
             short_text:'Korolki Orange', 
             href:'tomato-one.html',
+            img:'korolki-orange.jpg',
             desc:"Full desc"
         },
         {
@@ -27,6 +30,7 @@ var app = new Vue({
             title:"Articul: 21343", 
             short_text:'Pup Orange', 
             href:'tomato-one.html',
+            img:'Pup-orange.jpg',
             desc:"Full desc"
         },
         {
@@ -34,15 +38,42 @@ var app = new Vue({
             title:"Articul: 43423", 
             short_text:'Tomaswil Orange', 
             href:'tomato-one.html',
+            img:'Tomaswil-Orange.jpg',
             desc:"Full desc"
-        }]},
-   
-    // mounted:function(){
-    //     console.log(window.localStorage.getItem('prod'));
-    // },
-    // methods:{
-    //     addItem:function(id) {
-    //         window.localStorage.setItem('prod', id);
-    //     }
-    // }
+        }],
+        product: [],
+        btnVisible: false
+    },
+    created: function() {
+        this.getProduct();
+    },
+    methods: {
+        getProduct: function() {
+            var id = window.location.hash.replace('#', '');
+            for (var i = 0; i < this.products.length; i++) {
+                if (this.products[i].id === id) {
+                    this.product = this.products[i];
+                    break;
+                }
+            }
+        },
+        addToCart: function(id){
+            var cart = [];
+            if(window.localStorage.getItem('cart')){
+                cart = window.localStorage.getItem('cart').split(',');
+            }
+
+            if(cart.indexOf(String(id))){
+                cart.push(id);
+                window.localStorage.setItem('cart',cart.join());
+                this.btnVisible=true;
+            }
+        },
+        checkInCart:function(){
+            if(this.product && this.product.id && window.localStorage.getItem('cart').split(',').indexOf(String(this.product.id))!=-1) this.btnVisible=1;
+        } 
+       
+    }
+
+        
 });
